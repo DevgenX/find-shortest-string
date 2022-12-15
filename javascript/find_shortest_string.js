ea3cc1c69a82f928101daefebd468f321f6b3767
devgenx <sebastian110420@gmail.com>
2022-12-15 12:18:18 -0500

    commit tree ee5506c4958b0d9fcd7b88aae12185393cbe94c9

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,21 +1,25 @@
 function findShortestString(arr) {
   // type your code here
+  return arr.sort((a, b) => a.length - b.length);
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestString(["aaa", "a", "bb", "ccc"]));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestString(["cat", "hi", "dog", "an"]));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log(
+    "=>",
+    findShortestString(["flower", "juniper", "lily", "dadelion"])
+  );
 
   // BENCHMARK HERE
 }
